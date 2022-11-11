c52d221eb145b43f23261800251260826f498cf7
AntonHolden <robsmt@mail.ru>
2022-11-11 18:05:45 +0300

    hw_1 start

---
+++ HW_1/Class1/Task1/Rational.cs
@@ -0,0 +1,11 @@
+namespace Task1;
+
+public class Rational
+{
+    public readonly int Numerator { get; init; }
+    public readonly int Denominator { get; init; }
+    public Rational(int numerator, int denominator)
+    {
+        Numerator = numerator;        
+    }
+}
